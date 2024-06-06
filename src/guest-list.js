3701107bca38e97786b8daccc31f5628ba934b42
Meaghan Campbell <emeaghancampbell@gmail.com>
2024-06-06 19:35:37 -0400

    photo update

--- src/guest-list.js
+++ src/guest-list.js
@@ -21,6 +21,291 @@ const guestList = [
   [
     "Liam Campbell", 
     "Gracie Kinyon"
+  ],
+  [
+    "Connor McNary",
+    "Addy Vizvary"
+  ],
+  [
+    "Melissa Goldstein",
+    "Conor McCarthy"
+  ],
+  [
+    "Kira Sain",
+    "Alec Sain"
+  ],
+  [
+    "Katie Kloess",
+    "Jason Kloess"
+  ],
+  [
+    "Caitie Homa",
+    "Patrick Homa"
+  ],
+  [
+    "Holly Darnell"
+  ],
+  [
+    "Jazmine Quinn",
+    "Drew Pfotenhauer"
+  ],
+  [
+    "Becca Ambler"
+  ],
+  [
+    "Andrew Snyder",
+    "Alex Lima"
+  ],
+  [
+    "Candace Campbell"
+  ],
+  [
+    "Keith Campbell"
+  ],
+  [
+    "Matthew Campbell"
+  ],
+  [
+    "Craig Campbell",
+    "Julie Foley"
+  ],
+  [
+    "Kelly Campbell",
+    "Richard Lindsey"
+  ],
+  [
+    "Ella Campbell"
+  ],
+  [
+    "Sophia Campbell",
+    "Max Garrity-Janger"
+  ],
+  [
+    "Bill Ralston"
+  ],
+  [
+    "Olivia Campbell",
+    "Michael Soucy"
+  ],
+  [
+    "Emily Campbell",
+    "Derek Crowley"
+  ],
+  [
+    "Halle Trabue",
+    "Nick Teteris"
+  ],
+  [
+    "Caroline Duhan",
+    "Trent Duhan"
+  ],
+  [
+    "Conner Simpson",
+    "Danielle Simpson"
+  ],
+  [
+    "Frazier Robinson",
+    "Caroline Robinson"
+  ],
+  [
+    "Connor Benshoff",
+    "Madison Benshoff"
+  ],
+  [
+    "Liz Fortener"
+  ],
+  [
+    "Mike McNary",
+    "Bonnie Trabue"
+  ],
+  [
+    "Dana McGinnis"
+  ],
+  [
+    "Justin Wendel",
+    "Shannon Wendel",
+    "Gavin Wendel",
+    "Tye Wendel"
+
+  ],
+  [
+    "Greg Hamilton",
+    "Kellie Grant"
+  ],
+  [
+    "Anna McNary Starr"
+  ],
+  [
+    "Garth McNary",
+    "Sherry McNary",
+    "Ben McNary",
+    "Jamie McNary",
+    "Ross McNary"
+  ],
+  [
+    "Sally Hagans"
+  ],
+  [
+    "Lea Nuemeyer",
+    "Daniel Nuemeyer"
+  ],
+  [
+    "Jim Weber",
+    "Clark Weber"
+  ],
+  [
+    "Eric Parker",
+    "Vicki Parker"
+  ],
+  [
+    "Ryan Parker",
+    "Melinda Parker",
+    "Mackenzie Parker"
+  ],
+  [
+    "Alex Parker",
+    "Morgan Parker",
+    "Owen Parker"
+  ],
+  [
+    "Meredith Parker-Terry",
+    "Patrick Terry"
+  ],
+  [
+    "Lauren Mudd",
+    "Dave McDonald"
+  ],
+  [
+    "Corey Ganim",
+    "Jaquelyn Farrell"
+  ],
+  [
+    "Kyle Wilhelm"
+  ],
+  [
+    "Nick Fenger",
+    "Ivona Filic"
+  ],
+  [
+    "Nick Romano",
+    "Schafer Felix"
+  ],
+  [
+    "Sam Siomkos",
+    "Austin DeFilippo"
+  ],
+  [
+    "Courtney Long"
+  ],
+  [
+    "Tabatha Conley",
+    "Scott Conley"
+  ],
+  [
+    "Stephen Clark",
+    "Lynn Mayo"
+  ],
+  [
+    "Diane Dealy",
+    "Mike Dealy",
+    "Zach Dealy",
+    "Noah Dealy",
+    "Lucy Whitenight",
+    "Mills Austin"
+
+  ],
+  [
+    "Marylee Meadows",
+  ],
+  [
+    "Charles Mosteller",
+    "Alexa Mosteller"
+  ],
+  [
+    "Lisa Murphy",
+    "Shawn Murphy"
+  ],
+  [
+    "Andrew Smith"
+  ],
+  [
+    "Beth Ward",
+    "Wes Ward"
+  ],
+  [
+    "Elanore Smith"
+  ],
+  [
+    "Patrick Smith",
+    "Arden Lumpkin"
+  ],
+  [
+    "Brian Mitchell",
+    "Sydney Mitchell"
+  ],
+  [
+    "John Fitzmaurice",
+    "Betsy Fitzmaurice"
+  ],
+  [
+    "Hasan Mustafik",
+    "Kiley Delaney"
+  ],
+  [
+    "Gian Silvestri"
+  ],
+  [
+    "Clay Chapman",
+    "Meredith Chapman"
+  ],
+  [
+    "Hunter Bentson",
+    "Mackenzie Bentson"
+  ],
+  [
+    "Jesse Jordan",
+    "Cameron Jordan"
+  ],
+  [
+    "Colleen Pinyan",
+    "Clint Pinyan"
+  ],
+  [
+    "Rachel Fowler",
+    "Hart Fowler"
+  ],
+  [
+    "Buck Martin",
+    "Wendy Martin"
+  ],
+  [
+    "Nichole Smith",
+    "Jalen Gavin"
+  ],
+  [
+    "Sarah Christopher",
+    "Jacob Burt"
+  ],
+  [
+    "Brian Wendel",
+    "Kyle Wendel"
+  ],
+  [
+    "Will Murphy",
+    "Julia Murphy",
+    "Archie Murphy",
+    "Van Murphy"
+  ],
+  [
+    "Grayson Murphy",
+    "Whitney Murphy"
+  ],
+  [
+    "James Williams"
+  ],
+  [
+    "Cody Wendel",
+    "Nidhi Patel"
   ]
 ];
   
--- src/views/Photos.vue
+++ src/views/Photos.vue
@@ -2,32 +2,14 @@
     import Nav from '../components/Nav.vue'
     import { ref } from 'vue';
 
-    import photo1 from '../assets/gallery/photo1.jpg'
-    import photo2 from '../assets/gallery/photo2.jpg'
-    import photo3 from '../assets/gallery/photo3.jpg'
-    import photo4 from '../assets/gallery/photo4.jpg'
-    import photo5 from '../assets/gallery/photo5.jpg'
-    import photo6 from '../assets/gallery/photo6.jpg'
-    import photo7 from '../assets/gallery/photo7.jpg'
-    import photo8 from '../assets/gallery/photo8.jpg'
-    import photo9 from '../assets/gallery/photo9.jpg'
-    import photo10 from '../assets/gallery/photo10.jpg'
-    import photo11 from '../assets/gallery/photo11.jpg'
-    import photo12 from '../assets/gallery/photo12.jpg'
-    import photo14 from '../assets/gallery/photo14.jpg'
-    import photo15 from '../assets/gallery/photo15.jpg'
-    import photo16 from '../assets/gallery/photo16.jpg'
-    import photo17 from '../assets/gallery/photo17.jpg'
-    import photo18 from '../assets/gallery/photo18.jpg'
-    import photo19 from '../assets/gallery/photo19.jpg'
-    import photo20 from '../assets/gallery/photo20.jpg'
-    import photo21 from '../assets/gallery/photo21.jpg'
-    import photo22 from '../assets/gallery/photo22.jpg'
-    import photo23 from '../assets/gallery/photo23.jpg'
-    import photo24 from '../assets/gallery/photo24.jpg'
-    import photo25 from '../assets/gallery/photo25.jpg'
+    import photo1 from '../assets/gallery/katieludolphphotos-116.jpg'
+    import photo2 from '../assets/gallery/katieludolphphotos-131.jpg'
+    import photo3 from '../assets/gallery/katieludolphphotos-89.jpg'
+    import photo4 from '../assets/gallery/katieludolphphotos-36.jpg'
+    import photo5 from '../assets/gallery/katieludolphphotos-28.jpg'
 
-    const images = [photo1, photo2, photo3, photo4, photo5, photo6, photo7, photo8, photo9, photo10, photo11, photo12, photo14, photo15, photo16, photo17, photo18, photo19, photo20, photo21, photo22, photo23, photo24, photo25]
+
+    const images = [photo4, photo3, photo1, photo2, photo5]
     
     let loading = ref(true);
     let loadCounter = ref(0);
