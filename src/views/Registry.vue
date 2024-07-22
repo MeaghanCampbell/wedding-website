9027e294c21f74891bb3f773e43637fef7b2ad60
Meaghan Campbell <emeaghancampbell@gmail.com>
2024-07-22 17:37:15 -0400

    registry

--- src/views/Registry.vue
+++ src/views/Registry.vue
@@ -9,7 +9,7 @@
         <Nav />
 
         <section class="px-4 sm:px-8 py-14 sm:py-16 max-w-5xl mx-auto fade-in-smooth flex-grow">
-            <p class="text-3xl sm:text-4xl montserrat-light text-center blue">Registry <span class="playfair-italic">soon.</span></p>
+            <p class="text-3xl sm:text-4xl montserrat-light text-center blue">Registry coming <span class="playfair-italic">soon.</span></p>
         </section>
 
         <Footer class="mt-auto" />
